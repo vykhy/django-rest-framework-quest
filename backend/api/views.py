a1f03140f7543978675d7e5570a6b76a75589beb
vykhy <vikehietoyhoshii@gmail.com>
2023-12-22 16:09:08 +0530

    Product Serializer and Form

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,15 @@
 from django.forms.models import model_to_dict
-from django.http import JsonResponse
-from products.models import Product
 
+from products.models import Product
+from products.serializers import ProductSerializer
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
 def api_home(request, *args, **kwargs):
-    model_data = Product.objects.all().first()
-    return JsonResponse(model_to_dict(model_data))
+    instance = Product.objects.all().first()
+    data = ProductSerializer(instance).data
+    return Response(data)
+
+
