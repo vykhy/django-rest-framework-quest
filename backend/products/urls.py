6504f8c64519cb1870087754d59942a09ac4ac45
vykhy <vikehietoyhoshii@gmail.com>
2023-12-26 15:44:33 +0530

    Function based views as an alternative

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -4,5 +4,6 @@ from .import views
 
 urlpatterns = [
     path("", views.ProductListCreateAPIView.as_view()),
-    path('<int:pk>/', views.ProductDetailAPIView.as_view())
+    path('<int:pk>/', views.ProductDetailAPIView.as_view()),
+    path("all/", views.ProductListAPIView.as_view())
 ]
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,8 @@
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.http import Http404
+from django.shortcuts import get_object_or_404
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -26,3 +30,28 @@ class ProductListAPIView(generics.ListAPIView):
     '''
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+# Alternate function based view
+@api_view(['GET','POST'])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    method = request.method 
+    if method == 'GET':
+        if pk is not None:
+          obj = get_object_or_404(Product, pk=pk)
+          data = ProductSerializer(obj, many=False).data
+          return Response(data)
+          
+        queryset = Product.objects.all()
+        data = ProductSerializer(queryset, many=True).data
+        return Response(data)
+    elif method == 'POST':
+        serialized = ProductSerializer(data=request.data)
+        if serialized.is_valid(raise_exception=True):
+            # instance = serialized.save()
+            # instance = form.save()
+            content = serialized.validated_data.get('content') or None
+            if content is None:
+                content = serialized.validated_data.get('title')
+            serialized.save(content=content)
+            return Response(serialized.data)
+        return Response({'invalid': "not good data"}, status=400)
